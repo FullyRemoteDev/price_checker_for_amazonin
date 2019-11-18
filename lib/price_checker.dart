7716485ae9998a993be2ceea02a755a288ec2b0d
Jagannath Arumugam <jagannath.sysop@gmail.com>
2019-11-18 21:07:23 +0530

    adds FAB and ListView to add and list products
    removed the previous textbox, button and card widgets

--- lib/price_checker.dart
+++ lib/price_checker.dart
@@ -20,7 +20,10 @@ class PriceChecker extends StatefulWidget {
 class _PriceCheckerState extends State<PriceChecker> {
   TextEditingController urlTextController = TextEditingController();
   Scraper scraper = Scraper();
+
   List<String> productDetails = new List(5);
+
+  List<String> productList = [];
 
   void initState() {
     super.initState();
@@ -31,30 +34,13 @@ class _PriceCheckerState extends State<PriceChecker> {
     super.dispose();
   }
 
+  @override
   Widget build(BuildContext context) {
-    TextField urlTextField = TextField(
-      controller: urlTextController,
-      keyboardType: TextInputType.url,
-      textInputAction: TextInputAction.done,
-      decoration: InputDecoration(
-        border: OutlineInputBorder(),
-        labelText: 'Paste the amazon.in link:',
-        labelStyle: TextStyle(
-          fontSize: 21.0,
-          fontWeight: FontWeight.bold,
-        ),
-        helperText: 'eg. from the browser address bar',
-        helperStyle: TextStyle(
-          fontSize: 14.0,
-          fontStyle: FontStyle.italic,
-        ),
-      ),
-    );
 
     return Scaffold(
       appBar: AppBar(
         title: Text(
-          'Price Checker For Amazon India',
+          'Price Checker For Amazon.in',
           style: TextStyle(
             fontSize: 18.0,
           ),
@@ -63,43 +49,116 @@ class _PriceCheckerState extends State<PriceChecker> {
       ),
       body: Padding(
         padding: EdgeInsets.all(16.0),
-        child: Column(
-          children: <Widget>[
-            Container(
-              padding: EdgeInsets.all(12.0),
-              child: urlTextField,
-            ),
-            RaisedButton(
-              onPressed: () async {
-                productDetails =
-                    await scraper.urlFetcher(urlTextController.text);
-                setState(() {});
-              },
-              child: Text(
-                'Check Price',
-                style: TextStyle(
-                  fontSize: 21.0,
-                ),
-              ),
-            ),
-            Card(
-              child: InkWell(
-                splashColor: Colors.blue.withAlpha(30),
-                onTap: () {
-                  print('Card tapped.');
-                },
-                child: Container(
-                  padding: EdgeInsets.all(12.0),
-                  child: Text(
-                    '${productDetails[0]} - ₹ ${productDetails[1]}',
-                    textAlign: TextAlign.left,
-                  ),
-                ),
-              ),
-            ),
-          ],
+        child: _productListView(),
+//        Column(
+//          children: <Widget>[
+//            Card(
+//              child: InkWell(
+//                splashColor: Colors.blue.withAlpha(30),
+//                onTap: () {
+//                  print('Card tapped.');
+//                },
+//                child: Container(
+//                  padding: EdgeInsets.all(12.0),
+//                  child: Text(
+//                    '${productDetails[0]} - ₹ ${productDetails[1]}',
+//                    textAlign: TextAlign.left,
+//                  ),
+//                ),
+//              ),
+//            ),
+//          ],
+//        ),
+      ),
+
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          _productURL(context);
+        },
+        child: Icon(Icons.add),
+        backgroundColor: Colors.teal,
+      ),
+    );
+  }
+
+
+  _productURL(BuildContext context) {
+
+    TextField urlTextField = TextField(
+      controller: urlTextController,
+      keyboardType: TextInputType.url,
+      textInputAction: TextInputAction.done,
+      decoration: InputDecoration(
+        border: OutlineInputBorder(),
+        labelText: 'Paste here...',
+        labelStyle: TextStyle(
+          fontSize: 12.0,
+          fontWeight: FontWeight.bold,
         ),
       ),
+    );
+
+    return showDialog<void>(
+      context: context,
+      barrierDismissible: false, // user must tap button!
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: Text('Amazon.in Product Link'),
+          content: SingleChildScrollView(
+            child: Container(
+              padding: EdgeInsets.all(4.0),
+              child: urlTextField,
+            ),
+          ),
+          actions: <Widget>[
+            FlatButton(
+              child: Text('Cancel'),
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+            ),
+            FlatButton(
+              child: Text('Add'),
+              onPressed: () async {
+                productDetails =
+                await scraper.urlFetcher(urlTextController.text);
+                setState(() {
+                  productList.add('${productDetails[0]} - ₹ ${productDetails[1]}');
+                });
+                urlTextController.clear();
+                Navigator.of(context).pop();
+              },
+            ),
+          ],
+        );
+      },
+    );
+  }
+
+  Widget _productListView() {
+    return ListView.builder(
+      itemCount: productList.length,
+      itemBuilder: (context, index) {
+        final item = productList[index];
+        return Card(
+          child: ListTile(
+            title: Text(item),
+
+            onTap: () { //                                  <-- onTap
+              setState(() {
+                productList.insert(index, 'Planet');
+              });
+            },
+
+            onLongPress: () { //                            <-- onLongPress
+              setState(() {
+                productList.removeAt(index);
+              });
+            },
+
+          ),
+        );
+      },
     );
   }
 }
